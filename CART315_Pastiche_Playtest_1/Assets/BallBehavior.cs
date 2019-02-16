39cc3b27f7aafd8d2b04bd6d83f991776ba4eb75
Justinel2 <justine.lardeux@hotmail.com>
2019-02-16 13:36:46 -0500

    INDPROJECT_PT1: BallBehavior() add gravity/antigravity and current

--- CART315_Pastiche_Playtest_1/Assets/BallBehavior.cs
+++ CART315_Pastiche_Playtest_1/Assets/BallBehavior.cs
@@ -6,16 +6,16 @@ public class BallBehavior : MonoBehaviour {
 
 	public Rigidbody rb;
 	public Vector3 antiGravity = new Vector3(0,1,0);
-	public Vector3 currentLeft = new Vector3(-1,antiGravity.y/2,0);
-	public Vector3 currentRight = new Vector3(1,antiGravity.y/2,0)
+	public Vector3 currentLeft = new Vector3(-1,1/2,0);
+	public Vector3 currentRight = new Vector3(1,1/2,0);
 
-		// Use this for initialization
-		void Start () {
-		rb = GetComponent<Rigidbody>()
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
 	}
-
-		// Update is called once per frame
-		void Update () {
+	
+	// Update is called once per frame
+	void Update () {
 
 		if (transform.position.y <= 0) {
 			rb.useGravity = false;
