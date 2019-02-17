ebef0e11078e603b8344c3953aad0e147f41e3ba
Justinel2 <justine.lardeux@hotmail.com>
2019-02-16 19:46:03 -0500

    INDPROJ_PT1: Add sphere as pre/post synaptic neurons

---
+++ CART315_Pastiche_Playtest_1/Assets/GenerateBalls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GenerateBalls : MonoBehaviour {
+
+	int totalAmount = 20;
+
+	// Use this for initialization
+	void Start () {
+		GameObject prefab = Resources.Load ("neurotransmitter") as GameObject;
+		for (int i = 0; i < 20; i++) {
+			GameObject go = Instantiate (prefab) as GameObject;
+			go.transform.position = new Vector3 (Random.Range(-10.0f,10.0f), -4, 0);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
