500779f60c7f80eef891c0f6efb67b1cec0bb7d7
Justinel2 <justine.lardeux@hotmail.com>
2019-02-18 15:20:28 -0500

    INDPROJ_PT1: GenerateBalls() make two functions to invoke depending on time the individual and group generation

--- CART315_Pastiche_Playtest_1/Assets/BallBehavior.cs
+++ CART315_Pastiche_Playtest_1/Assets/BallBehavior.cs
@@ -4,33 +4,51 @@ using UnityEngine;
 
 public class BallBehavior : MonoBehaviour {
 
+	public GameObject postSynaptic;
+	public GameObject channelSerotonin;
 	public Rigidbody rb;
 	public SphereCollider postSCollider;
-	public GameObject postSynaptic;
-	private Vector3 velocity, exitVelocity; 
-	private float speed, ballRadius, postSRadius, postSAura, distance, totalRadius;
+	public CapsuleCollider channelSCollider;
+	private Vector3 entranceVelocity, velocity, exitVelocity; 
+	private float speed, ballRadius, channelSHeight, postSRadius, postSAura, distance, totalRadius;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		channelSerotonin = GameObject.Find("channel_serotonin");
+
 		ballRadius = GetComponent<SphereCollider>().radius * transform.lossyScale.x;
+		channelSHeight = channelSerotonin.GetComponent<CapsuleCollider> ().bounds.size.y;
 
 		postSynaptic = GameObject.Find("postsynaptic");
 		postSRadius = postSynaptic.GetComponent<SphereCollider> ().radius * postSynaptic.transform.lossyScale.y;
 
 		totalRadius = ballRadius + postSRadius;
-	
+
+		entranceVelocity = new Vector3 (0.0f,1.0f,0.0f);
 		velocity = new Vector3 (Random.Range (-1.5f, 1.5f), 1.0f, 0.0f);
 		exitVelocity = new Vector3 (2.0f, 1.0f, 0.0f);
+
+		speed = 1;
+
 	}
 	// Update is called once per frame
 	void Update () {
 //		
-		speed = Random.Range(0.5f, 1.5f);
-		rb.velocity = new Vector3 (velocity.x + Random.Range (-1.0f, 1.0f), velocity.y, velocity.z) * speed;
-
 		distance = Vector3.Distance(transform.position, postSynaptic.transform.position);
 		postSAura = Random.Range (-2.0f,0.2f);
+
+		if (transform.position.y <= channelSerotonin.transform.position.y + channelSHeight) {
+			speed = 2;
+			channelSerotonin.GetComponent<CapsuleCollider> ().enabled = false;
+			rb.velocity = entranceVelocity * speed;
+		}
+
+		if (transform.position.y > channelSerotonin.transform.position.y + channelSHeight) {
+			channelSerotonin.GetComponent<CapsuleCollider> ().enabled = true;
+			speed = Random.Range(0.5f, 1.5f);
+			rb.velocity = new Vector3 (velocity.x + Random.Range (-1.0f, 1.0f), velocity.y, velocity.z) * speed;
+		}
 
 		if (distance <= totalRadius + postSAura) {
 			if (transform.position.x <= 0) {
--- CART315_Pastiche_Playtest_1/Assets/GenerateBalls.cs
+++ CART315_Pastiche_Playtest_1/Assets/GenerateBalls.cs
@@ -1,24 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections.Generic;
+using System.Timers; 
 
 public class GenerateBalls : MonoBehaviour {
 
+	public GameObject neurotransmitter;
 	int totalAmount = 20;
 
 	// Use this for initialization
 	void Start () {
-		GameObject prefab = Resources.Load ("neurotransmitter") as GameObject;
-		for (int i = 0; i < 20; i++) {
-			GameObject go = Instantiate (prefab) as GameObject;
-			go.transform.position = new Vector3 (Random.Range(-10.0f,10.0f), -4, 0);
-		}
+		neurotransmitter = Resources.Load ("neurotransmitter") as GameObject;
+		InvokeRepeating("generateGroup", 0.0f, 8.0f);
+//		for (int i = 0; i < 5; i++) {
+//			GameObject ball = Instantiate (neurotransmitter) as GameObject;
+//			ball.transform.position = transform.position;
+//		}
 
 	}
 
 	// Update is called once per frame
 	void Update () {
+	}
 
+	void generateGroup() {
+		for (int i = 0; i < 5; i++) {
+			Invoke("generateInd", i*1.5f);
+		}
+	}
+
+	void generateInd() {
+		GameObject ball = Instantiate (neurotransmitter) as GameObject;
+		ball.transform.position = transform.position;
 	}
 }
