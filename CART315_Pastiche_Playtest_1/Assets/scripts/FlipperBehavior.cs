da1e710497f54a93be2eb5b5cf2f5b85c01065a5
Justinel2 <justine.lardeux@hotmail.com>
2019-02-18 16:41:51 -0500

    INDPROJ_PT1: Add flipper

---
+++ CART315_Pastiche_Playtest_1/Assets/scripts/FlipperBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperBehavior : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		HingeJoint hinge = GetComponent<HingeJoint>();
+
+		JointLimits limits = hinge.limits;
+
+
+		limits.min = 90;
+		limits.bounciness = 0;
+		limits.bounceMinVelocity = 0;
+		limits.max = 0;
+		hinge.limits = limits;
+		hinge.useLimits = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
