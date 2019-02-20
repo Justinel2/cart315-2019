cb3f9a9305f06c989ccee5f28f16be480755b63e
Justinel2 <justine.lardeux@hotmail.com>
2019-02-20 10:02:42 -0500

    INDPROJ_PT1: Instantiate right and left paddle when click on screen

---
+++ CART315_Pastiche_Playtest_1/Assets/scripts/FlipperGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperGenerator : MonoBehaviour {
+
+	Ray ray;
+	RaycastHit hit;
+	// Declare the game object neurotransmitter variable
+	public GameObject leftFlipper;
+	public GameObject rightFlipper;
+
+	// Use this for initialization
+	void Start () {
+		// Load the prefabs "neurotransmitter" as a game object
+		leftFlipper = Resources.Load ("left_flipper") as GameObject;
+		rightFlipper = Resources.Load ("right_flipper") as GameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if (Physics.Raycast (ray, out hit)) {
+
+			if (Input.GetKey (KeyCode.Mouse0)) {
+				if (hit.point.x <= 0) {
+					GameObject obj = Instantiate (leftFlipper, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+				}
+				if (hit.point.x > 0) {
+					GameObject obj = Instantiate (rightFlipper, new Vector3 (hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+				}
+			}
+		}
+	}
+}
+
