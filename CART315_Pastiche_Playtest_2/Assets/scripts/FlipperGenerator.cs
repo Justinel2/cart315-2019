cf1d895bd012c0045f9bfc593751e2ea024483f4
Justinel2 <justine.lardeux@hotmail.com>
2019-02-28 14:30:43 -0500

    P2: Starting Project 2

--- CART315_Pastiche_Playtest_2/Assets/scripts/FlipperGenerator.cs
+++ CART315_Pastiche_Playtest_2/Assets/scripts/FlipperGenerator.cs
@@ -6,20 +6,23 @@ public class FlipperGenerator : MonoBehaviour {
 
 	Ray ray;
 	RaycastHit hit;
+
 	// Declare the game object neurotransmitter variable
 	public GameObject leftFlipper;
 	public GameObject rightFlipper;
 	public GameObject channel;
 
+
 	private Vector3 startPoint;
 	private Vector3 endPoint;
+
 
 	// Use this for initialization
 	void Start () {
 		// Load the prefabs as a game objects
 		leftFlipper = Resources.Load ("left_flipper") as GameObject;
 		rightFlipper = Resources.Load ("right_flipper") as GameObject;
-
+	
 		channel = GameObject.Find("channel_serotonin");
 	}
 
@@ -35,7 +38,7 @@ public class FlipperGenerator : MonoBehaviour {
 			}
 			if (Input.GetKeyUp (KeyCode.Mouse0)) {
 				endPoint = Input.mousePosition;
-				Debug.DrawLine (startPoint, endPoint, Color.red);
+
 				if (startPoint.x < endPoint.x) {
 					GameObject obj = Instantiate (leftFlipper, new Vector3 (hit.point.x, hit.point.y, channel.transform.position.z), Quaternion.identity) as GameObject;
 				}
