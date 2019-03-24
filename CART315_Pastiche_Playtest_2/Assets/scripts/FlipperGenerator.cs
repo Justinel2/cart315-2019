b0f7e37dcfb78ecbfd58e7fd4f6743e95596b4a4
Justinel2 <justine.lardeux@hotmail.com>
2019-03-24 19:43:39 -0400

    INDPROJ_PT3: Left paddle angle handling

--- CART315_Pastiche_Playtest_2/Assets/scripts/FlipperGenerator.cs
+++ CART315_Pastiche_Playtest_2/Assets/scripts/FlipperGenerator.cs
@@ -11,13 +11,10 @@ public class FlipperGenerator : MonoBehaviour {
 	public GameObject leftFlipper;
 	public GameObject rightFlipper;
 	public GameObject channel;
+
+
 	private Vector3 startPoint;
 	private Vector3 endPoint;
-	private float distance;
-
-	private Vector3 angleReference;
-	private float limitMinAngle;
-	private float limitMaxAngle;
 
 
 	// Use this for initialization
@@ -27,8 +24,6 @@ public class FlipperGenerator : MonoBehaviour {
 		rightFlipper = Resources.Load ("right_flipper") as GameObject;
 	
 		channel = GameObject.Find("channel_serotonin");
-
-		angleReference = new Vector3 (0.0f,100.0f,0.0f);
 	}
 
 	// Update is called once per frame
@@ -43,33 +38,12 @@ public class FlipperGenerator : MonoBehaviour {
 			}
 			if (Input.GetKeyUp (KeyCode.Mouse0)) {
 				endPoint = Input.mousePosition;
-				distance = Vector3.Distance (startPoint, endPoint);
-
-				Vector3 direction = endPoint - startPoint;
-				float angle = Vector3.Angle(direction, angleReference);
-				print (angle);
-
-				if (angle < 90) {
-					limitMinAngle = -90;
-					limitMaxAngle = 0;
-				}
-				if (angle >= 90) {
-					angle = -angle;
-					limitMinAngle = 0;
-					limitMaxAngle = 90;
-				}
 
 				if (startPoint.x < endPoint.x) {
-					GameObject obj = Instantiate (leftFlipper, new Vector3 (hit.point.x, hit.point.y, channel.transform.position.z),   Quaternion.Euler(0, 0, angle)) as GameObject;
-					HingeJoint hj = obj.GetComponent<HingeJoint>();
-					JointLimits limits = hj.limits;
-					limits.min = limitMinAngle;
-					limits.max = limitMaxAngle;
-					print (limits.min);
-					hj.limits = limits;
+					GameObject obj = Instantiate (leftFlipper, new Vector3 (hit.point.x, hit.point.y, channel.transform.position.z), Quaternion.identity) as GameObject;
 				}
 				if (startPoint.x > endPoint.x) {
-					GameObject obj = Instantiate (rightFlipper, new Vector3 (hit.point.x, hit.point.y, channel.transform.position.z), Quaternion.Euler(0,0, angle)) as GameObject;
+					GameObject obj = Instantiate (rightFlipper, new Vector3 (hit.point.x, hit.point.y, channel.transform.position.z), Quaternion.identity) as GameObject;
 				}
 			}
 		}
